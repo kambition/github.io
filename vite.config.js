9b3ea8023acc5d3ed3a26e0deb90a5fdd637bb90
liulingkun <liulingkun@roborock.com>
2025-11-02 20:26:11 +0800

    升级 Node.js 到 20 并修复 Vite 配置

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // 如果你的仓库名是 ppt,就使用 '/ppt/'
-  // 如果你使用自定义域名或 username.github.io 仓库,就使用 '/'
-  base: './', // 使用相对路径,适用于所有情况
+export default {
+  // 使用相对路径,适用于所有情况
+  base: './',
   
   build: {
     outDir: 'dist',
@@ -14,4 +11,4 @@ export default defineConfig({
       }
     }
   }
-})
+}
